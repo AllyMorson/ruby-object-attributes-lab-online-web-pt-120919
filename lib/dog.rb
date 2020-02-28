7052d61aab43d136303f4af4579f1cc5bcff5ba9
Ally Morson <ally.morson@gmail.com>
2020-02-28 17:06:03 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+#class method 
+class Dog 
+
+ #instant method eg def bark "woof"
+ 
+  #instant variable @name = name 
+  def name=(name)
+    @name =  name 
+  end 
+  
+  def name 
+    @name
+  end 
+  
+  def breed= (breed)
+    @breed = breed 
+  end 
+  
+  def breed 
+    @breed 
+  end 
+end 
